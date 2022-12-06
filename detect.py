bc117c051659d0fcd9fdbfaa9f03dfc351798834
YuriyKarabintsev <114873039+YuriyKarabintsev@users.noreply.github.com>
2022-12-06 21:35:26 +0700

    Update detect.py
    Signed-off-by: YuriyKarabintsev <114873039+YuriyKarabintsev@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -177,11 +177,13 @@ def run(
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         print(label, "LABEL")
-                        out_file.write(label + "\n")
+                        out_file.write(label)
                         annotator.box_label(xyxy, label, color=colors(c, True))
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-
+                        
+            out_file.write("\n")
+                        
             # Stream results
             im0 = annotator.result()
             print(im0.shape[0], im0.shape[1], "IMAGE ZERO")
